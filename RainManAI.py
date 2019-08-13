cf4b366ad8d22f682e14c9b235c3ad3acc0c571c
Gregory Eales <gregoeales@aol.com>
2019-08-13 07:51:28 -0700

    AI change

--- RainManAI.py
+++ RainManAI.py
@@ -3,25 +3,7 @@ from matplotlib import pyplot
 from numpy import tanh
 
 
-class NeuralNetwork(object):
+class RainMainAI(object):
 
-    def __init__(self, weights_txt):
-        self.weights = self.load_weights(weights_txt)
-
-    def load_weights(self, weights_txt):
-
-        return weights_txt
-
-    def train(self, iterations):
-
-
-
-
-    def predict(self):
+    def __init__(self):
         pass
-
-    def back_prop(self):
-
-    def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
-

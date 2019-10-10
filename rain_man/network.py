e2816cf572ed0d37a60ce46c76895ea2c8c35ad1
Gregory Eales <gregoeales@aol.com>
2019-10-10 00:02:45 -0700

    added network

---
+++ rain_man/network.py
@@ -0,0 +1,15 @@
+import time
+
+import torch
+import numpy
+import tqdm
+
+
+class RainMainNet(torch.nn.Module):
+
+    def __init__(self):
+
+        super(self, RainMainNet).__init__()
+
+    def forward(self, x):
+        pass

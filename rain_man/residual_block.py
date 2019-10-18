545257a6fd88c233839ff18cb03db4d87df42d62
Gregory Eales <gregoeales@aol.com>
2019-10-17 20:47:29 -0700

    Create residual_block.py

---
+++ rain_man/residual_block.py
@@ -0,0 +1,21 @@
+import torch
+
+class Block(torch.nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(Block, self).__init__()
+        self.fc1 = torch.nn.Linear(input_size, input_size)
+        self.fc2 = torch.nn.Linear(input_size, input_size)
+        self.fc3 = torch.nn.Linear(input_size, input_size)
+        self.batch_norm = torch.nn.BatchNorm1d(input_size)
+        self.relu = torch.nn.LeakyReLU()
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.relu(out)
+        out = out + x
+        return out

861fcef0729b07087498791dd812200f02108fd3
Gregory Eales <gregoeales@aol.com>
2019-10-19 20:36:00 -0700

    Update texas_holdem.py

--- utils/texas_holdem.py
+++ utils/texas_holdem.py
@@ -1,4 +1,5 @@
 import time
+from random import shuffle
 
 import torch
 import numpy
@@ -9,6 +10,9 @@ class Card(object):
         self.suit = suit
         self.rank = rank
 
+    def __str__(self):
+        return "{} of {}".format(self.rank, self.suit)
+
 class Deck(object):
 
     def __init__(self):
@@ -16,43 +20,95 @@ class Deck(object):
         self.ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack",
          "Queen", "King", "Ace"]
 
+        self.cards = []
+        self.populate_deck()
+        self.shuffle()
+
+    def populate_deck(self):
+        for suit in self.suits:
+            for rank in self.ranks:
+                self.cards.append(Card(suit, rank))
+
     def reset(self):
-        pass
+        self.cards = []
+        self.populate_deck
 
     def shuffle(self):
-        pass
+        shuffle(self.cards)
 
-class Player(self):
+
+    def draw_card(self):
+        card = self.cards[0]
+        self.cards.remove(card)
+        return card
+
+class Player(object):
 
     def __init__(self):
 
         self.chips = 0
-        self.cards = 0
+        self.cards = []
+
+    def add_cards(self, card1, card2):
+        self.cards.append(card1)
+        self.cards.append(card2)
+
+    def reset_cards(self):
+        self.cards = []
+
+    def __str__(self):
+        card1 = self.cards[0].__str__()
+        card2 = self.cards[1].__str__()
+        return "Chips:{}, Cards: {}, {}".format(self.chips, card1, card2)
 
 class TexasHoldem(object):
 
     def __init__(self, num_players=2):
 
         self.num_players = num_players
-
+        self.players = {}
         self.community_cards = []
         self.pot = 0
+        self.deck = Deck()
 
 
-    def draw_card(self):
-        pass
+        # game logic
+        self.is_betting = True
+        self.dealer = "P1"
 
-    def reset_deck(self):
-        pass
+        self.initialize_players()
+
 
     def step(self, action):
-        # actions: bet, raise, fold
+        """
+         actions: bet:-1, fold:-2, raise:[0, 1]
+         raise: between 0 and 1, represent percentage of chips to be raised
 
+        """
+        # return possible actions [bet, fold, raise]
         # returns next player_id, cards, community cards, all player chips, pot
         pass
 
-    def reset(self, action):
-        pass
+    def initialize_players(self):
+
+        for player in range(self.num_players):
+            self.players["P"+str(player)] = Player()
+
+    def draw_card(self):
+        return self.deck.draw_card()
+
+    def reset_deck(self):
+        self.deck.reset()
+
+    def remove_player_cards(self):
+
+        for i in range(self.num_players):
+            self.players["P"+str(i)].reset_cards
+
+    def reset_players(self):
+        self.players = {}
+        for player in range(self.num_players):
+            self.players["P"+str(player)] = Player()
 
     def check_highcard(self):
         pass
@@ -83,3 +139,11 @@ class TexasHoldem(object):
 
     def check_royal_flush(self):
         pass
+
+def main():
+    a = Deck()
+    card = a.draw_card()
+
+
+if __name__ == "__main__":
+    main()
